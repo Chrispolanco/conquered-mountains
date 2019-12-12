4eb860ac164e47afc9d0025529154880ae6a9cb8
Christian_Polanco <chrispolanco@sbcglobal.net>
2019-12-11 22:48:08 -0800

    controller routes basic

--- app/controllers/mountains_controller.rb
+++ app/controllers/mountains_controller.rb
@@ -1,2 +1,36 @@
 class MountainsController < ApplicationController
-end
+    def index 
+
+    end 
+
+    def show 
+
+    end
+
+    def new 
+
+    end 
+
+    def crete
+
+    end 
+
+    def edit 
+    
+    end 
+
+    def update 
+
+    end 
+
+    def destroy 
+
+    end 
+
+    private 
+
+        def climber_params
+            params.require(:mountains).permit(:mountain_name, :route, :difficulty_rating)
+        end 
+
+end 
