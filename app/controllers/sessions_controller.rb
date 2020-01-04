3a60f2db1ab1b5699701f631c7476ac74bdfc971
Christian_Polanco <chrispolanco@sbcglobal.net>
2020-01-04 13:17:53 -0800

    added destroy in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,4 +11,11 @@ class SessionsController < ApplicationController
         end 
     end 
 
+    def destroy 
+        session.delete(:user_id)
+        redirect_to login_path
+    end 
+
+    
+
 end
