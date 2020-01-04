1b2898ed2c1dde56dd181f9d471e13e4a684eb1a
Christian_Polanco <chrispolanco@sbcglobal.net>
2020-01-04 13:16:30 -0800

    added create in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,14 @@
 class SessionsController < ApplicationController
+
+    def create 
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id 
+            redirect_to user_path(@user)
+        else
+            fash[:error] = "Login not successful"
+            redirect_to login_path 
+        end 
+    end 
+
 end
