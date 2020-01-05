83b8d075d26b32167047779eb4cdc435e571af57
Christian_Polanco <chrispolanco@sbcglobal.net>
2020-01-05 00:54:29 -0800

    deleted index users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,6 @@ class UsersController < ApplicationController
 
     before_action :set_user, only: [:show, :edit, :update, :destory]
 
-    def index 
-        @users = User.all 
-    end 
-
     def show 
 
     end
--- app/models/climb.rb
+++ app/models/climb.rb
@@ -2,6 +2,5 @@ class Climb < ApplicationRecord
     belongs_to :climber
     belongs_to :mountain
 
+    validates :name, :duration, :date_started, :date_completed, presence: true
 end
-
-
